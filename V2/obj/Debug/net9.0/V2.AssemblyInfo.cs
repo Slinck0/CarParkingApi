01deb70b85a8e679586d3bbd6d2bdeaf0fecdcb4
Rens2002 <159784770+Rens2002@users.noreply.github.com>
2025-10-29 09:02:00 +0100

    test

--- V2/obj/Debug/net9.0/V2.AssemblyInfo.cs
+++ V2/obj/Debug/net9.0/V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c739f068db0ed5fed8324ecaf10f567b9651b6cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466a9e5c2bd8d9a9e918af6588a84944c04c85e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
