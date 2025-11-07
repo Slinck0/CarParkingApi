01aa5819891590fd20fe87ecd31ea20a4f942a87
Rens2002 <159784770+Rens2002@users.noreply.github.com>
2025-11-07 11:46:48 +0100

    Database_vehicle

--- V2/api/AppInt.cs
+++ V2/api/AppInt.cs
@@ -1,35 +1,45 @@
 namespace Jsonimporter.Api;
+ 
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ParkingImporter.Data;
 using ParkingImporter.Import;
-public static  class AppInt
+public static class AppInt
 {
     public static async Task ImportJson()
     {
         var options = new DbContextOptionsBuilder<AppDbContext>()
         .UseSqlite("Data Source=parking.db").Options;
-
+ 
         using var db = new AppDbContext(options);
-
-        
-
+        await db.Database.ExecuteSqlRawAsync("PRAGMA journal_mode=WAL;");
+        await db.Database.ExecuteSqlRawAsync("PRAGMA synchronous=NORMAL;");
+ 
+ 
+ 
         var lotsPath = "parking-lots.json";
         var reservationsPath = "reservations.json";
         var usersPath = "users.json";
         var vehiclesPath = "vehicles.json";
         var paymentsPath = "payments.json";
-
+ 
         if (File.Exists(usersPath))
             await UsersImporter.ImportAsync(db, usersPath);
-
+ 
         if (File.Exists(vehiclesPath))
             await VehiclesImporter.ImportAsync(db, vehiclesPath);
+ 
+        if (File.Exists(lotsPath))
+            await LotsImporter.ImportAsync(db, lotsPath);
+ 
+        if (File.Exists(reservationsPath))
+            await ReservationsImporter.ImportAsync(db, reservationsPath);
+ 
         if (File.Exists(paymentsPath))
             await PaymentsImporter.ImportAsync(db, paymentsPath);
-        
-
+ 
+ 
         Console.WriteLine("Alle imports gereed ✅");
-
+ 
     }
 }
