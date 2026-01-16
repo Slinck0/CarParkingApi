e73f708f2b69be2cf23a925bc8a9000a5c237e87
Rens2002 <159784770+Rens2002@users.noreply.github.com>
2026-01-16 11:34:57 +0100

    Revert "Rens"
    This reverts commit 6fddbfb60cde0a12ea5e0b76c857a37d62ff56db.

--- V2/Program.cs
+++ V2/Program.cs
@@ -2,7 +2,8 @@ using V2.Endpoints;
 using V2.Services;
 using V2.Data;
 using V2.Api;
-using V2.Models; // Zorg dat je de namespace van UserModel hebt
+using V2.Models;
+using V2.Helpers; // Zorg dat je de namespace van UserModel hebt
 
 public class Program
 {
@@ -26,38 +27,11 @@ public class Program
             var services = scope.ServiceProvider;
             var db = services.GetRequiredService<AppDbContext>();
 
-            
+
             db.Database.EnsureCreated();
 
-
-            {
-                db.Users.Add(new UserModel
-                {
-                    Username = "Rens",
-                    Password = BCrypt.Net.BCrypt.HashPassword("Rens"),
-                    Name = "Rens Admin",
-                    Email = "rens@test.nl",
-                    Phone = "0612345678",
-                    Role = "ADMIN",
-                    Active = true,
-                    CreatedAt = DateOnly.FromDateTime(DateTime.Now),
-                    BirthYear = 1990
-                });
-                db.SaveChanges();
-            }
-            if (!db.ParkingLots.Any(p => p.Id == 1))
-        {
-            db.ParkingLots.Add(new ParkingLotModel 
-            {
-                Id = 1,
-                Name = "Test Garage CI",
-                Location = "Rotterdam",
-                Address = "Stationstraat 1",
-                Capacity = 100,
-                Tariff = 2.50m,
-                Status = "Open"
-            });
-        }
+            AdminHelper.AddAdminIfNotExists(db);
+            AdminHelper.AddParkinglotIfNotExists(db);
 
             // 3. Je bestaande import logica
             if(args.Contains("import"))
