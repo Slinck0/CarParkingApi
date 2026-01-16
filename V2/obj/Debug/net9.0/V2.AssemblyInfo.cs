16b6502328f104bc83ab5980e7ec273567de0fb3
OlivierS4242 <112190173+OlivierS4242@users.noreply.github.com>
2026-01-16 10:12:48 +0100

    Gitignore Improvement.

--- V2/obj/Debug/net9.0/V2.AssemblyInfo.cs
+++ V2/obj/Debug/net9.0/V2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+010b7eeda753b2cddaedd7eb3994f4049ab9b2fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e52603e91711118ce13155786c92167d460cddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
