1e778875b0100134798f5cb7d577cdab20989c2e
Rens2002 <159784770+Rens2002@users.noreply.github.com>
2026-01-16 11:40:20 +0100

    Revert "m"
    This reverts commit 81f1f92a64872429f797e144ed6afae9d9ceae9d.

--- ParkingApi.Tests/obj/Debug/net9.0/ParkingApi.Tests.AssemblyInfo.cs
+++ ParkingApi.Tests/obj/Debug/net9.0/ParkingApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkingApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0ea7948e92282d07348539b38f6b073beae381")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2266ccbde729bf0ff94e1d4b1a67862f482ab523")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkingApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkingApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
